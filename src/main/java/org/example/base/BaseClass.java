82b1e07abfbb1e77cd183f37f95dd932817c1ff3
SujalSkya <125185426+SujalSkya@users.noreply.github.com>
2023-05-01 22:48:29 +0545

    Update BaseClass.java

--- src/main/java/org/example/base/BaseClass.java
+++ src/main/java/org/example/base/BaseClass.java
@@ -22,31 +22,24 @@ public class BaseClass {
     public static void lunchApp() throws MalformedURLException {
 
         DesiredCapabilities cap = new DesiredCapabilities();
-        cap.setCapability("platformName", "Android");
-        cap.setCapability("appium:platformVersion", "12.0");
+        cap.setCapability("platformName", "<enter platform for sender>");
+        cap.setCapability("appium:platformVersion", "<enter sender device version>");
         cap.setCapability("appium:automationName", "uiautomator2");
-        cap.setCapability("appium:deviceName", "Galaxy M51");
+        cap.setCapability("appium:deviceName", "<enter sender device name>");
         cap.setCapability("appPackage", "com.khalti");
         cap.setCapability("appActivity", "com.khalti.MainActivity");
-        cap.setCapability("udid", "RZ8NC03VM3A");
+        cap.setCapability("udid", "<enter sender device serial number>");
 
 
         DesiredCapabilities cap1 = new DesiredCapabilities();
-        cap1.setCapability("platformName", "Android");
+        cap1.setCapability("platformName", "<enter platform for receiver>");
         cap1.setCapability("appium:platformVersion", "12.0");
         cap1.setCapability("appium:automationName", "uiautomator2");
-        cap1.setCapability("appium:deviceName", "Redmi 10 Prime");
-        cap1.setCapability("udid","aab43fe10511");
+        cap1.setCapability("appium:deviceName", "<receiver device name>");
+        cap1.setCapability("udid","<receiver device serial number>");
         cap1.setCapability("appPackage", "com.khalti");
         cap1.setCapability("appActivity", "com.khalti.MainActivity");
 
-//        DesiredCapabilities cap1 = new DesiredCapabilities();
-//        cap1.setCapability("platformName", "Android");
-//        cap1.setCapability("avd", "Pixel_4_API_31");
-//        cap1.setCapability("platformVersion", "12");
-//        cap1.setCapability("appium:automationName", "uiautomator2");
-//        cap1.setCapability("appPackage", "com.khalti");
-//        cap1.setCapability("appActivity", "com.khalti.MainActivity");
 
         URL url = new URL("http://127.0.0.1:4723/");
 
